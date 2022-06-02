e2ae031dc03090752c366f31e29cfc9eaae69977
ElijahS67 <97219114+ElijahS67@users.noreply.github.com>
2022-06-02 16:22:39 +0800

    added scripts

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+
+{
+    /*
+    public PlayerScript movement;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("platformUnderside"))
+        {
+            isUndersidePlatform = true;   
+            RB.gravityScale = -30f;
+        }
+        
+        if(collision.gameObject.CompareTag("ground"))
+        {
+            RB.gravityScale = 30f;
+            isUndersidePlatform = false;
+            
+            if(isOnGround == false)
+            {
+                isOnGround = true;
+            }
+
+            
+        }
+
+        if(collision.gameObject.CompareTag("spike") || collision.gameObject.CompareTag("enemy"))
+         {
+             FindObjectOfType<GameManager>().GameOver();
+            
+    
+         }
+    }
+    */
+}
