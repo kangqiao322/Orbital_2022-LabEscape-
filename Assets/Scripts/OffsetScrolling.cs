21d37be66ad593a30d5510689b6bc808941690bc
ElijahS67 <97219114+ElijahS67@users.noreply.github.com>
2022-07-03 00:25:50 +0800

    background transition basics done

--- Assets/Scripts/OffsetScrolling.cs
+++ Assets/Scripts/OffsetScrolling.cs
@@ -6,28 +6,86 @@ public class OffsetScrolling : MonoBehaviour {
     public float scrollSpeed;
     public float maxSpeed;
 
-    public GameManager gameManager;
+    [SerializeField] private GameManager gameManager;
+    [SerializeField] private Material[] materialArray = new Material[2];
 
-    private Material renderer;
+    private Material currentMaterial;
     private Vector2 savedOffset;
 
-    private void Awake () {
-        renderer = GetComponent<Renderer>().material;
+    private float timeToNextBackground = 5f;
+    private float timePassed = 0f;
+    
+    private Queue<Material> queue = new Queue<Material>();
+
+    private void Start() {
+        foreach (Material m in materialArray)
+        {
+            queue.Enqueue(m); //FIFO
+        }
+
+        // MeshRenderer a = GetComponent<MeshRenderer>();
+        // Debug.Log(a.materials[1]);
+
+        //GetComponent<MeshRenderer>().materials[0] = null;
+        
+
+        currentMaterial = queue.Dequeue(); //a queue is first in, first out FIFO
+        GetComponent<MeshRenderer>().material = currentMaterial;
+        //StartCoroutine(scrollBackground());
     }
+
+    // private IEnumerator scrollBackground()
+    // {
+    //     maxSpeed = gameManager.getMaxSpeed() * 0.025f;
+    //     scrollSpeed = gameManager.getSpeed() * 0.025f;
+    //
+    //     while (true)
+    //     {
+    //         if (scrollSpeed < maxSpeed) 
+    //         {
+    //             scrollSpeed += Time.deltaTime * 0.01f;
+    //         }
+    //
+    //         savedOffset = new Vector2(scrollSpeed, 0);
+    //         renderer.mainTextureOffset += savedOffset * Time.deltaTime;
+    //     }
+    //     
+    //     // while (scrollSpeed < maxSpeed) 
+    //     // {
+    //     //     scrollSpeed += Time.deltaTime * 0.01f;
+    //     //     savedOffset = new Vector2(scrollSpeed, 0);
+    //     //     renderer.mainTextureOffset += savedOffset * Time.deltaTime;
+    //     // }
+    //     //
+    //     // Debug.Log("maxSpeed now");
+    //     //
+    //     // savedOffset = new Vector2(scrollSpeed, 0);
+    //     // renderer.mainTextureOffset += savedOffset * Time.deltaTime;
+    // }
 
     void Update () {
         //correlate to game manager
-    maxSpeed = gameManager.getMaxSpeed() * 0.025f;
-    scrollSpeed = gameManager.getSpeed() * 0.025f;
+        maxSpeed = gameManager.getMaxSpeed() * 0.025f;
+        scrollSpeed = gameManager.getSpeed() * 0.025f;
+        
+        timePassed += Time.deltaTime;
+        
+        if (scrollSpeed < maxSpeed) 
+        {
+            scrollSpeed += Time.deltaTime * 0.01f;
+        }
+        
+        savedOffset = new Vector2(scrollSpeed, 0);
+        currentMaterial.mainTextureOffset += savedOffset * Time.deltaTime;
+
+        if (timePassed > timeToNextBackground)
+        {
+            timePassed = 0;
+            queue.Enqueue(currentMaterial); //puts it back
+            currentMaterial = queue.Dequeue(); //take new one
+            GetComponent<MeshRenderer>().material = currentMaterial; //displays new one in unity
+            //the material in mesh renderer is the one that is displaying the the background
+        }
     
-    if (scrollSpeed < maxSpeed) 
-    {
-        scrollSpeed += Time.deltaTime * 0.01f;
-    }
-  
-
-    savedOffset = new Vector2(scrollSpeed, 0);
-    renderer.mainTextureOffset += savedOffset * Time.deltaTime;
-
     }
 }
